b1444268584c36f2373b238300cab8caed7b4fd4
mnescio <morris.md0@gmail.com>
2021-06-03 12:03:59 -0400

    back to tf 1.15.0

--- load_text_generator.py
+++ load_text_generator.py
@@ -23,7 +23,7 @@ def Generate(one_step_model, N_characters, seed):
       next_char, states = one_step_model.generate_one_step(next_char, states=states)
       result.append(next_char)
 
-	output = hub.tf_utils.tf.strings.join(result)[0].numpy().decode('UTF-8')
+    output = hub.tf_utils.tf.strings.join(result)[0].numpy().decode('UTF-8')
     #string = ''
     #for c in np.array(result):
     #    string = string + str(c[0])[2:-1]
