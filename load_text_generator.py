559fc790d26efff104babe1c2d6992d1682cdfd8
mnescio <morris.md0@gmail.com>
2021-06-03 05:29:01 -0400

    adding tensorflow hub back in

--- load_text_generator.py
+++ load_text_generator.py
@@ -2,19 +2,20 @@
 # coding: utf-8
 
 
-import tensorflow as tf
-# tf version 1.15.0
+#import tensorflow as tf
+# tf version 2.4.0
 
+import tensorflow hub as hub
 import numpy as np
 
-one_step_model = tf.saved_model.load('test_model')
+one_step_model = hub.tf_utils.tf.saved_model.load('test_model')
     
     
 def Generate(one_step_model, N_characters, seed):
     states = None
 
-    #next_char = hub.tf_utils.tf.constant(seed)
-    next_char = tf.constant(seed)
+    next_char = hub.tf_utils.tf.constant(seed)
+    #next_char = tf.constant(seed)
 
 
     result = [next_char]
