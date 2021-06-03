0bb8d097fb5b8a3869793878662a2465c1f067e9
mnescio <morris.md0@gmail.com>
2021-06-03 03:44:34 -0400

    back to tf 2.3.0

--- load_text_generator.py
+++ load_text_generator.py
@@ -27,7 +27,7 @@ def Generate(one_step_model, N_characters, seed):
 
     output = np.array(hub.tf_utils.tf.strings.join(result))[0].decode('UTF-8')
     
-    print(output)
+    #print(output)
     return output
 
 
